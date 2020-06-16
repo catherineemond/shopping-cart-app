ded4071c667f2c2f418fbfc4499b08421c098b92
Mark Hyams <markhyams@gmail.com>
2020-06-16 17:33:40 -0600

    extracted form component working

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -2,72 +2,16 @@ import React from "react";
 import Form from './Form.js'
 
 class AddForm extends React.Component {
-  state = {
-    title: '',
-    price: '', 
-    quantity: '' 
-  }
-
-  handleOnChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.onAddProduct(this.state) 
-    this.setState({
-      title: '',
-      price: '', 
-      quantity: '' 
-    })
-  }
-
   render() {
     return (
+      // add/remove 'visible' from class to hide/show button
       <div className="add-form visible">
-        <p>
-          <a className="button add-product-button">Add A Product</a>
-        </p>
+        <p><a class="button add-product-button">Add A Product</a></p>
         <h3>Add Product</h3>
-        <Form  onChange={this.handleOnChange} onSubmit={this.handleSubmit}/>
-        {/* <form>
-          <div className="input-group">
-            <label htmlFor="product-name">Product Name</label>
-            <input type="text" id="product-name" name='title' value={this.state.title} onChange={this.handleOnChange} />
-          </div>
-
-          <div className="input-group">
-            <label htmlFor="product-price">Price</label>
-            <input type="text" id="product-price" name='price' value={this.state.price} onChange={this.handleOnChange}/>
-          </div>
-
-          <div className="input-group">
-            <label htmlFor="product-quantity">Quantity</label>
-            <input type="text" id="product-quantity" name='quantity' value={this.state.quantity} onChange={this.handleOnChange}/>
-          </div>
-
-          <div className="actions form-actions">
-            <a className="button" onClick={this.handleSubmit}>Add</a>
-            <a className="button">Cancel</a>
-          </div>
-        </form> */}
+        <Form onAddProduct={this.props.onAddProduct} />
       </div>
     );
   }
 }
-
-// const AddForm = (props) => {
-//   if (props.isOpen) {
-   
-//   } else {
-//     return (
-//       <p>
-//         <a className="button add-product-button">Add A Product</a>
-//       </p>
-//     );
-//   }
-// };
 
 export default AddForm;
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -2,9 +2,29 @@ import React from 'react'
 
 class Form extends React.Component {
   state = {
-    title: this.props.title, 
-    price: this.props.price, 
-    quantity: this.props.quantity
+    title: this.props.title || "", 
+    price: this.props.price || "", 
+    quantity: this.props.quantity || "",
+  }
+
+  // moved from AddForm
+  handleOnChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  // moved from AddForm
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAddProduct(this.state) 
+
+    // reset form
+    this.setState({
+      title: '',
+      price: '', 
+      quantity: '' 
+    })
   }
 
   render() {
@@ -12,21 +32,21 @@ class Form extends React.Component {
       <form>
         <div className="input-group">
           <label htmlFor="product-name">Product Name</label>
-          <input type="text" id="product-name" name='title' value={this.state.title} onChange={this.props.handleOnChange} />
+          <input type="text" id="product-name" name='title' value={this.state.title} onChange={this.handleOnChange} />
         </div>
 
         <div className="input-group">
           <label htmlFor="product-price">Price</label>
-          <input type="text" id="product-price" name='price' value={this.state.price} onChange={this.props.handleOnChange}/>
+          <input type="text" id="product-price" name='price' value={this.state.price} onChange={this.handleOnChange}/>
         </div>
 
         <div className="input-group">
           <label htmlFor="product-quantity">Quantity</label>
-          <input type="text" id="product-quantity" name='quantity' value={this.state.quantity} onChange={this.props.handleOnChange}/>
+          <input type="text" id="product-quantity" name='quantity' value={this.state.quantity} onChange={this.handleOnChange}/>
         </div>
 
         <div className="actions form-actions">
-          <a className="button" onClick={this.props.handleSubmit}>Add</a>
+          <a className="button" onClick={this.handleSubmit}>Add</a>
           <a className="button">Cancel</a>
         </div>
       </form>
